3d2782b63db8496267363963f3ae508bab10eccd
aidan-casey <aidan-casey@users.noreply.github.com>
2022-05-25 16:16:31 +0000

    Fix styling

--- tests/ViewModelRouteBindingTest.php
+++ tests/ViewModelRouteBindingTest.php
@@ -31,9 +31,9 @@ class ViewModelRouteBindingTest extends TestCase
     {
         $app['config']->set('database.default', 'testbench');
         $app['config']->set('database.connections.testbench', [
-            'driver'   => 'sqlite',
+            'driver' => 'sqlite',
             'database' => ':memory:',
-            'prefix'   => '',
+            'prefix' => '',
         ]);
         $app['config']->set('app.debug', true);
     }
